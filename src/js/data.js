12a899789efa295210109bada582e11ffca8f20c
Macclarenz <89197798+Macclarenz@users.noreply.github.com>
2022-04-03 17:53:41 +0800

    Add files via upload

---
+++ src/js/data.js
@@ -0,0 +1,7 @@
+module.exports = userData = [
+    {username: 'tif', password: 'TiF<>123'},
+    {username: 'josh', password: 'JoSh#21'},
+    {username: 'nathan', password: 'nathanWhat?'},
+    {username: 'kit', password: 'tikKit'},
+    {username: 'kali', password: 'kali321@'}
+]
