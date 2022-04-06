bbd0df4ec0c8c082c0157d7af337f8970faf7a24
Mac clarenz Tamang <macclarenz@gmail.com>
2022-04-06 12:33:07 +0800

    reset

--- src/js/data.js
+++
@@ -1,8 +0,0 @@
-export default  [
-    {username: 'tif', password: 'TiF<>123'},
-    {username: 'josh', password: 'JoSh#21'},
-    {username: 'nathan', password: 'nathanWhat?'},
-    {username: 'kit', password: 'tikKit'},
-    {username: 'kali', password: 'kali321@'},
-    {username: 'null', password: 'undefined'}
-]
